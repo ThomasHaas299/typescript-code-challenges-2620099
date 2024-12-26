d8e96c41a3dfbb040be37994fb7fd146593e3fc7
Thomas Haas <github@thomashaas.de>
2024-12-26 16:12:10 +0000

    Lösung 01_01 Kommentar verlinkt

--- 01_01_Tuple/Challenge_Tuple.ts
+++ 01_01_Tuple/Challenge_Tuple.ts
@@ -1,12 +1,16 @@
 // Task 1:
-type XYTuple = any;
+type XYTuple = [number, number];
 
 const XY: XYTuple = [12, 39];
 const XY_Wrong: XYTuple = [12, 39, 20];
 
 // Task 2:
-type NameAgeHobbyTuple = any;
+// Spread-Operator:  https://www.typescriptlang.org/docs/handbook/variable-declarations.html#spread
+type NameAgeHobbyTuple = [string, number, string, ...string[]];
 const NameAgeHobby: NameAgeHobbyTuple = ["Anny", 25, "Surfing"];
+const NameAgeHobby1: NameAgeHobbyTuple = ["Anny", 25, "Surfing", "Reading"];
+
 const NameAgeHobby_Wrong: NameAgeHobbyTuple = ["Anny", 25];
+const NameAgeHobby_Wrong1: NameAgeHobbyTuple = ["Anny", 25, 30];
 
 export {};
